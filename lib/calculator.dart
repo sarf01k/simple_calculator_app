31f1bf2642a79804e1882b991b471b5618d8b43e
sarf01k <isaacsarfo2004@gmail.com>
2023-03-22 03:54:10 +0000

    added functionality and ui

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class SimpleCalculator extends StatefulWidget {
   const SimpleCalculator({super.key});
@@ -8,75 +9,134 @@ class SimpleCalculator extends StatefulWidget {
 }
 
 class _SimpleCalculatorState extends State<SimpleCalculator> {
+
+  var input = "";
+  var output = "";
+  var operation = "";
+
+  onButtonClick(value) {
+    if (value == "C") {
+      input = "";
+    }
+    else if (value == "⌫") {
+      if (input.isNotEmpty) {
+      input = input.substring(0, input.length - 1);
+      }
+    }
+    else if (value == "=") {
+      if (input.isNotEmpty) {
+        var userInput = input;
+        userInput= input.replaceAll("x", "*");
+        userInput= input.replaceAll("÷", "/");
+        Parser p = Parser();
+        Expression expression = p.parse(userInput);
+        ContextModel cm = ContextModel();
+        var finalValue = expression.evaluate(EvaluationType.REAL, cm);
+        output = finalValue.toString();
+        if (output.endsWith(".0")) {
+          output = output.substring(0, output.length -2);
+        }
+        input = output;
+      }
+    }
+    else {
+      input = input + value;
+    }
+
+    setState(() {
+
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                color: Colors.greenAccent,
-              )
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            input,
+            style: TextStyle(fontSize: 50, color: Colors.white, fontWeight: FontWeight.bold),
             ),
-            Container(
-              padding: EdgeInsets.only(top: 30, right: 18, left: 18, bottom: 0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(45)
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      button(),button(),button(),button()
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      button(),button(),button(),button()
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      button(),button(),button(),button()
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      button(),button(),button(),button()
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      button(),button(),button(),button()
-                    ],
-                  ),
-                ],
-              ),
+          SizedBox(height: 5,),
+          Text(
+            output,style: TextStyle(fontSize: 36, color: Colors.grey.withOpacity(0.7), fontWeight: FontWeight.bold),),
+          SizedBox(height: 20),
+          Container(
+            padding: const EdgeInsets.only(top: 30, right: 18, left: 18, bottom: 0),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+              color: Color(0xFF222222),
             ),
-          ],
-        ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    button(text: "C", buttonBgColor: Color(0xFF434242), textColor: Colors.green),
+                    button(text: "%", buttonBgColor: Color(0xFF434242), textColor: Colors.green),
+                    button(text: "⌫", buttonBgColor: Color(0xFF434242), textColor: Colors.green),
+                    button(text: "÷", buttonBgColor: Color(0xFF434242), textColor: Colors.red)
+                  ],
+                ),
+                Row(
+                  children: [
+                    button(text: "7", buttonBgColor: Color(0xFF434242),),
+                    button(text: "8", buttonBgColor: Color(0xFF434242),),
+                    button(text: "9", buttonBgColor: Color(0xFF434242),),
+                    button(text: "x", buttonBgColor: Color(0xFF434242), textColor: Colors.red)
+                  ],
+                ),
+                Row(
+                  children: [
+                    button(text: "4", buttonBgColor: Color(0xFF434242),),
+                    button(text: "5", buttonBgColor: Color(0xFF434242),),
+                    button(text: "6", buttonBgColor: Color(0xFF434242),),
+                    button(text: "-", buttonBgColor: Color(0xFF434242), textColor: Colors.red)
+                  ],
+                ),
+                Row(
+                  children: [
+                    button(text: "1", buttonBgColor: Color(0xFF434242),),
+                    button(text: "2", buttonBgColor: Color(0xFF434242),),
+                    button(text: "3", buttonBgColor: Color(0xFF434242),),
+                    button(text: "+", buttonBgColor: Color(0xFF434242), textColor: Colors.red)
+                  ],
+                ),
+                Row(
+                  children: [
+                    button(text: "⁺⁄₋", buttonBgColor: const Color(0xFF434242),),
+                    button(text: "0", buttonBgColor: Color(0xFF434242),),
+                    button(text: ".", buttonBgColor: const Color(0xFF434242),),
+                    button(text: "=", buttonBgColor: const Color(0xFF434242), textColor: Colors.red)
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
-    Widget button(){
+    Widget button({text, buttonBgColor = const Color(0xFF434242), textColor = Colors.white}) {
       return Expanded(
         child: Container(
-          margin: EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
           child: ElevatedButton(
             style: ElevatedButton.styleFrom(
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(12)
               ),
-              padding: EdgeInsets.symmetric(horizontal: 14, vertical: 22),
-              backgroundColor: Color(0xFF354249)
+              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 22),
+              backgroundColor: buttonBgColor
             ),
-            onPressed: (){},
+            onPressed: () => onButtonClick(text),
             child: Text(
-              "1",
-              style: TextStyle(fontSize: 25),
-            )),
+              text,
+              style: TextStyle(color: textColor, fontSize: 25, fontWeight: FontWeight.bold),
+            )
+          ),
         )
       );
     }
